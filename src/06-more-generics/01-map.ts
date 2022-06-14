ad9f3afa95922a63b0535fe8efe3316a8f569e2d
Albert G <516972+alber70g@users.noreply.github.com>
2022-06-14 10:30:05 +0200

    Update 01-map.ts

--- src/06-more-generics/01-map.ts
+++ src/06-more-generics/01-map.ts
@@ -6,10 +6,10 @@ const map = () => {};
 // ===============================
 // Don't change below these lines
 // ===============================
-const A: void[] = theMap([''], () => {});
-const B: number[] = theMap([''], (_val, i) => i);
-const C: string[][] = theMap([''], (_val, _i, arr) => arr);
-const D: string[] = theMap(['string', { type: 'object' }], x => {
+const A: void[] = map([''], () => {});
+const B: number[] = map([''], (_val, i) => i);
+const C: string[][] = map([''], (_val, _i, arr) => arr);
+const D: string[] = map(['string', { type: 'object' }], x => {
   if (typeof x === 'string') {
     return x;
   } else {
